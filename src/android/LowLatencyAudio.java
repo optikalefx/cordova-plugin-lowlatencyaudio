7fe329063b991dd3f6f79fd098870a9ee7806c9f
Sean Clark <sean@square-bracket.com>
2014-07-17 14:47:47 -0400

    Update LowLatencyAudio.java

--- src/android/LowLatencyAudio.java
+++ src/android/LowLatencyAudio.java
@@ -67,14 +67,14 @@ public class LowLatencyAudio extends CordovaPlugin {
 				Log.d(LOGTAG, "preloadFX - " + audioID + ": " + assetPath);
 
 				int assetIntID = 0;
-				if(assetPath.startsWith("/")) {
-				    assetIntID = soundPool.load(assetPath,1);
-				} else {
-				    Context ctx = cordova.getActivity().getApplicationContext();
-				    AssetManager am = ctx.getResources().getAssets();
-				    AssetFileDescriptor afd = am.openFd("www/" + assetPath);
-				    assetIntID = soundPool.load(afd, 1);
-				}
+				//if(assetPath.startsWith("/")) {
+				    assetIntID = soundPool.load(assetPath, 1);
+				//} else {
+				//    Context ctx = cordova.getActivity().getApplicationContext();
+				//    AssetManager am = ctx.getResources().getAssets();
+				//    AssetFileDescriptor afd = am.openFd("www/" + assetPath);
+				//    assetIntID = soundPool.load(afd, 1);
+				//}
 				soundMap.put(audioID, assetIntID);
 			} else {
 				return new PluginResult(Status.ERROR, ERROR_AUDIOID_EXISTS);
